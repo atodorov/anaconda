165c61dd7127dcd047adcea9af824f86b20a9c7f
Alexander Todorov <atodorov@redhat.com>
2016-04-12 15:24:06 +0300

    Define missing srcdir in run_gui_tests.sh

--- tests/run_gui_tests.sh
+++ tests/run_gui_tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+srcdir=${srcdir:=$(dirname "$0")}
+
 if ! rpm -q python3-nose-testconfig &> /dev/null; then
     echo "python3-nose-testconfig is not available; exiting."
     exit 99
@@ -7,6 +9,7 @@ fi
 
 export LC_ALL=C # translations confuse Dogtail
 
-exec ${srcdir}/nosetests.sh -s --nologcapture --process-timeout 1200 \
+exec COVERAGE_PROCESS_START=${srcdir}/../.coveragerc \
+    ${srcdir}/nosetests.sh -s --nologcapture --process-timeout 1200 \
     --tc=resultsdir:$(readlink -f autogui-results-$(date +"%Y%m%d_%H%M%S")) \
     ${srcdir}/gui/test_*.py
